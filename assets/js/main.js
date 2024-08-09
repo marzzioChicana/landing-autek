4c66ceee0cb666736826453f0f844b01f5d92802
marzzioChicana <u202022228@upc.edu.pe>
2024-08-09 14:19:32 -0500

    add nosotros section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
 /*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) =>{
-   const toggle = document.getElementById(toggleId),
-         nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId),
+          navItems = nav.querySelectorAll('a');
 
-   toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
-       nav.classList.toggle('show-menu')
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu');
 
-       // Add show-icon to show and hide the menu icon
-       toggle.classList.toggle('show-icon')
-   })
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon');
+    });
+
+    // Cerrar el menú al hacer clic en una opción
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            // Remove show-menu class from nav menu
+            nav.classList.remove('show-menu');
+
+            // Remove show-icon to hide the menu icon
+            toggle.classList.remove('show-icon');
+        });
+    });
 }
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu');
 
 
 
